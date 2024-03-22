e4d7ddea1fd9c83c225599c7f15ab829060b94b8
james2323123 <36201181+james2323123@users.noreply.github.com>
2024-03-22 08:43:13 +0800

    Update kernels.py

--- nets/kernels.py
+++ nets/kernels.py
@@ -4,16 +4,14 @@ import random
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-# 計算任兩點的歐氏距離 (weight matrix)
+# calculates squared euclidean distance between points
 def euclidean_matrix(Q, K):
-    return torch.sqrt(
+    return (
     torch.sum(Q ** 2, dim=-1).unsqueeze(-1)
     +torch.sum(K ** 2, dim=-1).unsqueeze(-2)
     -2*torch.matmul(Q, K.transpose(-2, -1))
     )
 
-# normalize方法，其實就是把資料映射到 [a,b] ，公式則是參考 wiki
-# https://en.wikipedia.org/wiki/Normalization_(statistics)
 def normalize(G, a=-9, b=9):
     max_vals = torch.max(torch.max(G, dim=-1)[0], dim=-1)[0]
     min_vals = torch.min(torch.min(G, dim=-1)[0], dim=-1)[0]
@@ -24,27 +22,27 @@ def vanilla_attention(Q, K, a=9):
 
 def cauchy(Q, K, a=9):
     dist = euclidean_matrix(Q,K)
-    compatibility = 1 / (1 + dist.pow(2))
+    compatibility = 1 / (1 + dist)
     return compatibility
 
 def normalized_cauchy(Q, K, a=9):
     dist = euclidean_matrix(Q,K)
-    compatibility = 1 / (1 + dist.pow(2))
+    compatibility = 1 / (1 + dist)
     return normalize(compatibility, -a, a)
 
 def clipped_cauchy(Q, K, a=9):
     dist = euclidean_matrix(Q,K)
-    compatibility = 1 / (1 + dist.pow(2))
+    compatibility = 1 / (1 + dist)
     return torch.clamp(compatibility,-a,a)
 
 def exponential(Q, K, a=9):
     dist = euclidean_matrix(Q,K)
-    compatibility = torch.exp(-dist)
+    compatibility = torch.exp(-torch.sqrt(torch.abs(dist)))
     return compatibility
 
 def gaussian(Q, K, a=9):
     dist = euclidean_matrix(Q,K)
-    compatibility = torch.exp(-(dist ** 2))
+    compatibility = torch.exp(-dist)
     return compatibility
 
 kernels = {
